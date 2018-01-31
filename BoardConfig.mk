8c7af3aab569aaafcdc6cbe4fb04bd5d84ed0072
Gautham <gauthamasir@gmail.com>
2018-01-31 22:36:13 +0530

    Revert Audio Hal

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,9 +42,6 @@ TARGET_KERNEL_HEADER_ARCH := arm
 # PowerHAL
 TARGET_POWERHAL_VARIANT := samsung
 
-# AudioHAl
-TARGET_AUDIOHAL_VARIANT := samsung
-
 # Extracted with libbootimg
 #BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt $(DEVICE_PATH)/dt.img --board SRPOJ08A000KU
