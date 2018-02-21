f3f357102a8949cc1537f137ac44034c59cef9c7
Gautham <gauthamasir@gmail.com>
2018-02-21 23:24:50 +0530

    Use magisk for root

--- aosp_j7eltexx.mk
+++ aosp_j7eltexx.mk
@@ -22,6 +22,9 @@ $(call inherit-product, vendor/aosp/common.mk)
 # Include Bootanimation configuration
 TARGET_BOOT_ANIMATION_RES := 720
 
+# Use Magisk
+DEFAULT_ROOT_METHOD := magisk
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := aosp_j7eltexx
 PRODUCT_RELEASE_NAME := SM-J700F
